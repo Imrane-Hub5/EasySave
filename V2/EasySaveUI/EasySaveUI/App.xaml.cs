b75c0ef06a25f1f0e98ee114def7286d8bfe93f3
Imrane-Hub5 <imrane.maadoum@gmail.com>
2026-05-11 08:48:26 +0200

    feat: add App.xaml.cs
    Refactor App class to include OnStartup method for initialization.

--- V2/EasySaveUI/EasySaveUI/App.xaml.cs
+++ V2/EasySaveUI/EasySaveUI/App.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System.Configuration;
-using System.Data;
 using System.Windows;
 
-namespace EasySaveUI;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace EasySave
 {
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Global initialization logic here
+        }
+    }
 }
-
